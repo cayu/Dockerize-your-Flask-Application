9a17a5a8bf2a86e2a663126bc77201c171dbd9e3
Cayu <cayu@cayu.com.ar>
2021-06-04 01:10:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
 if __name__ == '__main__':
     app.run(port=5000,debug=True,host='0.0.0.0')
