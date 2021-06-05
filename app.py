8f092e6103104a5016d2ace6e19ccc7311e18b8a
Cayu <cayu@cayu.com.ar>
2021-06-05 02:39:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-import time
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
-
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True,host='0.0.0.0')
