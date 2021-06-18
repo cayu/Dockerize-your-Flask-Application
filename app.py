458a71648e490b5b6e24fe0589671cce92bd771a
Cayu <cayu@cayu.com.ar>
2021-06-18 10:29:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
+@app.route('/name/<name>', methods=['GET'])
+def name(name):
+    return 'Hello %s' + name
+
 if __name__ == '__main__':
     app.run(port=5000,debug=True,host='0.0.0.0')
