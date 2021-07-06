b79acc36a05a555d2ce9b69b4a190b13af368f21
Cayu <cayu@cayu.com.ar>
2021-07-06 15:45:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,69 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# /// = relative path, //// = absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+
+class Todo(db.Model): # pylint: disable=too-few-public-methods
+    """A dummy docstring."""
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-@app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    def pub1(self):
+        """A dummy docstring."""
+        print("")
 
-@app.route('/name/<name>', methods=['GET'])
-def name(name):
-    return 'Hello ' + name
+    def pub2(self):
+        """A dummy docstring."""
+        print("")
 
-@app.route('/edit')
-def edit()
+@app.route("/edit")
+def home1():
+    """A dummy docstring."""
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
 
-@app.route('/all')
-def all():
+
+@app.route("/")
+def list1():
+    """A dummy docstring."""
     todo_list = Todo.query.all()
     return render_template("list.html", todo_list=todo_list)
 
-@app.route('/add', method['POST'])
-def add()
-    title=request.form.get('title')
-    new_todo= Todo(title=title, complete=_False)
+
+@app.route("/add", methods=["POST"])
+def add():
+    """A dummy docstring."""
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for("edit"))
+    return redirect(url_for("home1"))
 
 
-@app.route('/base')
-def base():
-    return render_template("base.html")
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    """A dummy docstring."""
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home1"))
 
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    """A dummy docstring."""
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home1"))
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(port=5000,debug=True,host='0.0.0.0')
